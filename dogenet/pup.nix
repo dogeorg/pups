0d81177ce208063aa13ff8f25b71c7c8697344dd
Raffe <andrew@dogecoin.com>
2024-10-24 12:10:18 +1100

    dogenet: 0.1.6
    Removed GeoIP database from dogenet, now in dogemap-backend

--- dogenet/pup.nix
+++ dogenet/pup.nix
@@ -2,15 +2,14 @@
 
 let
   dogenet_upstream = pkgs.callPackage (pkgs.fetchurl {
-    url = "https://raw.githubusercontent.com/dogeorg/dogebox-nur-packages/22e2e8041f9d857861c9b60bcefd6f0291fa5c56/pkgs/dogenet/default.nix";
-    sha256 = "sha256-fSjv8kJ5VCZ5nUlZpBqmNu/sS4wLU/glinOT7Axao8A=";
+    url = "https://raw.githubusercontent.com/dogeorg/dogebox-nur-packages/0fe8f2bb6d1fb842f51145d87adb04d89e81046a/pkgs/dogenet/default.nix";
+    sha256 = "sha256-7AvXs1EUt66qiddSuVzVMEn0JZWmcObhRtEIEKcdQyE=";
   }) {};
 
   dogenet = pkgs.writeScriptBin "run.sh" ''
     #!${pkgs.bash}/bin/bash
     export KEY=`cat /storage/delegated.key`
     IP=`${pkgs.curl}/bin/curl https://reflector.dogecoin.org/me | ${pkgs.jq}/bin/jq -r .ip`
-    cp ${dogenet_upstream}/bin/storage/dbip-city-ipv4-num.csv /storage
     ${dogenet_upstream}/bin/dogenet --handler ''${DBX_PUP_IP}:42068 --web ''${DBX_PUP_IP}:8080 --public ''${IP}:42069 --dir /storage
   '';
 in
