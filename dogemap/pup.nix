cc1e3dc66466b78ca1dc7dee3fa2f3df357e8ee2
Raffe <andrew@dogecoin.com>
2024-10-24 12:05:22 +1100

    dogemap: 0.0.6 and backend 0.1.2

--- dogemap/pup.nix
+++ dogemap/pup.nix
@@ -1,20 +1,21 @@
 { pkgs ? import <nixpkgs> {} }:
 
 let
-  jampuppy = pkgs.callPackage (pkgs.fetchurl {
-    url = "https://raw.githubusercontent.com/dogeorg/dogebox-nur-packages/0b01b0f9a537e44d0faf105abd879ea8dfac04ca/pkgs/jampuppy/default.nix";
-    sha256 = "sha256-467MLF9QKhj1ah823AUzQCL9zhMWzbIPf6bfhZqtvMw=";
+  dogemap_upstream = pkgs.callPackage (pkgs.fetchurl {
+    url = "https://raw.githubusercontent.com/dogeorg/dogebox-nur-packages/0fe8f2bb6d1fb842f51145d87adb04d89e81046a/pkgs/dogenet/default.nix";
+    sha256 = "sha256-7AvXs1EUt66qiddSuVzVMEn0JZWmcObhRtEIEKcdQyE=";
   }) {};
 
   ui = pkgs.fetchgit {
     url = "https://github.com/dogeorg/dogemap-ui.git";
-    rev = "v0.0.2";
-    sha256 = "sha256-qITc1EcEYvHOeVszztd7G3l04AJfu/wzOapRokMvndk=";
+    rev = "v0.0.6";
+    sha256 = "sha256-W1BU52P/6WM3zUhna1PWWb+of8KqZ3hY/cs9brJlmUQ=";
   };
 
   dogemap = pkgs.writeScriptBin "run.sh" ''
     #!${pkgs.bash}/bin/bash
-    ${jampuppy}/bin/jampuppy -A index.html --dir ${ui} --host 0.0.0.0 --port 8080 --proxy "/dogenet http://''${DBX_IFACE_DOGENET_WEB_API_HOST}:''${DBX_IFACE_DOGENET_WEB_API_PORT}/"
+    cp ${dogemap_upstream}/storage/dbip-city-ipv4-num.csv /storage
+    ${dogemap_upstream}/bin/dogemap --bind ''${DBX_PUP_IP}:8091 --dir /storage --web ${ui} --core ''${DBX_IFACE_CORE_NETWORK_HOST}:''${DBX_IFACE_CORE_NETWORK_PORT} --dogenet ''${DBX_IFACE_DOGENET_WEB_API_HOST}:''${DBX_IFACE_DOGENET_WEB_API_PORT} --identity ''${DBX_IFACE_IDENTITY_WEB_API_HOST}:''${DBX_IFACE_IDENTITY_WEB_API_PORT}
   '';
 in
 {
